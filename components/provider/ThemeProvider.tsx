c1300a910ecd8e71498d03a9c7da9d6c9c3291cf
Sardar25 <mubasharmumtaz029@gmail.com>
2025-02-02 17:35:03 +0500

    bugs fixes

--- components/provider/ThemeProvider.tsx
+++ components/provider/ThemeProvider.tsx
@@ -1,11 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  // State to determine whether we are on the client
+  const [mounted, setMounted] = React.useState(false);
+
+  // Set mounted to true when the component has mounted on the client
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // If not mounted (i.e., on the server), return nothing to prevent mismatches
+  if (!mounted) {
+    return null;
+  }
+
+  // Return the ThemeProvider when mounted on the client
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
